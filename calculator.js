7bcd29be94ebf562f7197f434d7fc7c1a2b3b0f6
Andrew-Bao <andrewbaoq3@gmail.com>
2022-09-29 22:56:31 -0400

    added display features

--- calculator.js
+++ calculator.js
@@ -6,3 +6,38 @@ const calculator = {
     waitingForSecondOperand: false,
     operator: null,
   };
+
+function updateDisplay() {
+  const display = document.querySelector('.calculator-screen');
+  display.value = calculator.displayValue;
+}
+
+updateDisplay();
+
+const keys = document.querySelector('.calculator-keys');
+keys.addEventListener('click', (event) => {
+  const {target} = event;
+
+  if (!target.matches('button')) {
+    return;
+  }
+
+  if (target.classList.contains('operator')) {
+    console.log('operator', target.value);
+    return;
+  }
+
+  if (target.classList.contains('decimal')) {
+    console.log('decimal', target.value);
+    return;
+  }
+
+  if (target.classList.contains('all-clear')) {
+    console.log('clear', target.value);
+    return;
+  }
+
+  console.log('digit', target.value);
+});
+
+const {target} = event;

4df623669dfa51832018bd9c810e5130b0651fb9
Andrew-Bao <andrewbaoq3@gmail.com>
2022-09-30 13:43:54 -0400

    handle operator functions

--- calculator.js
+++ calculator.js
@@ -23,13 +23,14 @@ keys.addEventListener('click', (event) => {
   }
 
   if (target.classList.contains('operator')) {
-    console.log('operator', target.value);
+    handleOperator(target.value);
+    updateDisplay();
     return;
   }
 
   if (target.classList.contains('decimal')) {
-    console.log('decimal', target.value);
-    return;
+    inputDecimal(target.value);
+    updateDisplay();
   }
 
   if (target.classList.contains('all-clear')) {
@@ -37,7 +38,43 @@ keys.addEventListener('click', (event) => {
     return;
   }
 
-  console.log('digit', target.value);
+  inputDigit(target.value);
+  updateDisplay();
 });
 
 const {target} = event;
+
+function inputDigit(digit) {
+  const { displayValue } = calculator;
+  // Overwrite `displayValue` if the current value is '0' otherwise append to it
+  calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+
+  console.log(calculator);
+}
+function inputDecimal(dot) {
+  // If the `displayValue` property does not contain a decimal point
+  if (!calculator.displayValue.includes(dot)) {
+    // Append the decimal point
+    calculator.displayValue += dot;
+  }
+}
+
+function handleOperator(nextOperator) {
+  // Destructure the properties on the calculator object
+  const { firstOperand, displayValue, operator } = calculator
+  // `parseFloat` converts the string contents of `displayValue`
+  // to a floating-point number
+  const inputValue = parseFloat(displayValue);
+
+  // verify that `firstOperand` is null and that the `inputValue`
+  // is not a `NaN` value
+  if (firstOperand === null && !isNaN(inputValue)) {
+    // Update the firstOperand property
+    calculator.firstOperand = inputValue;
+  }
+
+  calculator.waitingForSecondOperand = true;
+  calculator.operator = nextOperator;
+
+  console.log(calculator);
+}
